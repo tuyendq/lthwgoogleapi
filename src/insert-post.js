e18f8d789ad80f2df5bbd4d27f8caab92cad827a
Tuyen Dang <tuyendq@gmail.com>
2020-01-07 22:08:34 +0700

    update insert-post.js

--- src/insert-post.js
+++ src/insert-post.js
@@ -1,1 +1,20 @@
 const {google} = require('googleapis')
+const dotenv = require('dotenv').config()
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.OAUTH_CLIENT_ID,
+    process.env.OAUTH_CLIENT_SECRECT,
+    process.env.OAUTH_REDIRECT_UR
+)
+
+// Generate a url that asks permissions for Blogger and more
+const scopes = [
+    'https://www.googleapis.com/auth/blogger'
+]
+
+const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'offline',
+    // If you only need one scope you can pass it as a string
+    scope: scopes
+})
